147c4398042185d83734169044932b5f78ab8e8e
hanopcan <hannah@opcan.co.uk>
2021-07-27 16:38:53 +0100

    Tidy up formatting of not gree result and social share

--- templates/checker/checker-result-not-green.php
+++ templates/checker/checker-result-not-green.php
@@ -13,22 +13,27 @@
 
 	<div class="tgwf-result__summary">
 		<img class="tgwf-result__smiley" src="<?php echo URL_DIR . 'public/img/green-web-smiley-bad.svg' ?>" alt="Website hosted grey">
-
-		<div>
-			<p><?php _e( 'Unfortunately, this website is hosted grey or we are not sure about the greenness of the hoster. For the check to work, (technical) information about green hosters worldwide is needed. This information is stored in the Green Hosts database.</p>
-			<p>Do you believe this information is incorrect?', 'tgwf' ); ?> <a href="https://admin.thegreenwebfoundation.org">Login to our database.</a></p>
-		</div>
 	</div>
 
 	<div class="">
-		<strong>Show the world this website is not green.</strong><br>							
-		<img src="https://api.thegreenwebfoundation.org/greencheckimage/<?php echo$green_check_result->url; ?>" alt="This website is hosted grey - checked by thegreenwebfoundation.org">
-		<p>Save this image or use the code below to implement this badge on your website.</p>
-		<pre><?php echo htmlspecialchars('<img src="https://api.thegreenwebfoundation.org/greencheckimage/') .$green_check_result->url . htmlspecialchars('" alt="This website is hosted Green - checked by thegreenwebfoundation.org">');?></pre>
-
-		<?php $excerpt =$green_check_result->url . ' is not hosted green.'; ?>
-		<span>Maybe you want to share this result with the hosting company? This creates leverage for your call to become green!<br></span>
+		<h3>Show the world this website is not green.</h3>
+		
+		<div>
+			<p>Unfortunately, this website is hosted grey or we are not sure about the greenness of the hoster.</p>
+			<p>For the check to work, (technical) information about green hosters worldwide is needed. This information is stored in our <a href="https://www.thegreenwebfoundation.org/green-web-datasets/">Green Web database</a>.</p>
+			<p>Do you believe this information is incorrect? <a href="https://admin.thegreenwebfoundation.org">Login to our database.</a></p>
+		</div>
 	</div>
+</div>
+
+<div class="">
+	<strong>Save this image or use the code below to implement this badge on your website.</strong></p>
+	<img src="https://api.thegreenwebfoundation.org/greencheckimage/<?php echo$green_check_result->url; ?>" alt="This website is hosted grey - checked by thegreenwebfoundation.org">
+	
+	<pre><?php echo htmlspecialchars('<img src="https://api.thegreenwebfoundation.org/greencheckimage/') .$green_check_result->url . htmlspecialchars('" alt="This website is hosted Green - checked by thegreenwebfoundation.org">');?></pre>
+
+	<?php $excerpt =$green_check_result->url . ' is not hosted green.'; ?>
+	<span>Maybe you want to share this result with the hosting company? This creates leverage for your call to become green!<br></span>
 </div>
 
 <?php require_once PDEV_DIR . 'templates/checker/checker-social.php'; ?>
