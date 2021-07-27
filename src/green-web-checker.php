00aca7cd4a4f346a88f68cec9c869bc1e3fb76eb
hanopcan <hannah@opcan.co.uk>
2021-07-27 16:38:39 +0100

    Implement supporting doc output in green results

--- src/green-web-checker.php
+++ src/green-web-checker.php
@@ -23,3 +23,16 @@ function tgwf_run_site_check() {
 
 	return $green_check_result;
 }
+
+/**
+ * Perform a hosting companu check.
+ */
+function tgwf_run_hosting_company_check( $host_id ) {
+	$hosting_company_result = '';
+
+	// Request and decode the response.
+	$response               = wp_remote_get( 'https://api.thegreenwebfoundation.org/data/hostingprovider/' . $host_id . '?nocache=true', [ 'timeout' => 10 ] );
+	$hosting_company_result = json_decode( wp_remote_retrieve_body( $response ) );
+
+	return $hosting_company_result;
+}
