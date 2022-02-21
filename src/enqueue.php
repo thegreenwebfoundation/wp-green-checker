d82c397bd3d6934755bdfe352dd16e94598404a3
hanopcan <hannah@opcan.co.uk>
2022-02-21 13:55:54 +0000

    Enqueue CSS for shortcode properly so it's not inlined

--- src/enqueue.php
+++ src/enqueue.php
@@ -6,17 +6,13 @@
  */
 
 add_action( 'wp', 'tgwf_register_scripts' );
-add_action( 'wp_footer', 'tgwf_register_shortcode_styles' );
+add_action( 'wp_enqueue_scripts', 'tgwf_load_shortcode_styles' );
 
 
 /**
  * Initialises the registration of scripts.
  */
 function tgwf_register_scripts() {
-
-	if ( is_front_page() ) {
-		add_action( 'wp_enqueue_scripts', 'tgwf_enqueue_home_scripts' );
-	}
 
 	if ( is_page( 'directory' ) ) {
 		add_action( 'wp_enqueue_scripts', 'tgwf_enqueue_directory_scripts' );
@@ -25,13 +21,6 @@ function tgwf_register_scripts() {
 	if ( is_page( 'green-web-check' ) ) {
 		add_action( 'wp_enqueue_scripts', 'tgwf_enqueue_greencheck_scripts' );
 	}
-}
-
-/**
- * Loads the scripts and styles required for the homepage.
- */
-function tgwf_enqueue_home_scripts() {
-	//wp_enqueue_script( 'tgwf-scripts', URL_DIR . 'public/js/tgwf.js' , array( 'tgwf-jquery', 'tgwf-moment' ), '1.0', false );
 }
 
 /**
@@ -44,9 +33,9 @@ function tgwf_enqueue_directory_scripts() {
 
 	// Enqueue scripts.
 	wp_enqueue_script( 'tgwf-directory', URL_DIR . 'public/js/directory.js', array( 'jquery' ), '1.0', true );
-	wp_enqueue_script( 'tgwf-scripts', URL_DIR . 'public/js/tgwf.js' , array('jquery'), '', false );		
-	wp_enqueue_script( 'tgwf-vmap', URL_DIR . 'public/js/jquery.vmap.js' , array('jquery'), '1.0', true );
-	wp_enqueue_script( 'tgwf-vmap-world', URL_DIR . 'public/js/maps/jquery.vmap.world.js' , array('jquery'), '1.0', true );	
+	wp_enqueue_script( 'tgwf-scripts', URL_DIR . 'public/js/tgwf.js', array( 'jquery' ), '', false );
+	wp_enqueue_script( 'tgwf-vmap', URL_DIR . 'public/js/jquery.vmap.js', array( 'jquery' ), '1.0', true );
+	wp_enqueue_script( 'tgwf-vmap-world', URL_DIR . 'public/js/maps/jquery.vmap.world.js', array( 'jquery' ), '1.0', true );
 }
 
 
@@ -57,35 +46,19 @@ function tgwf_enqueue_greencheck_scripts() {
 
 	// Enqueue styles.
 	wp_enqueue_style( 'tgwf-checker', URL_DIR . 'public/css/tgwfchecker.css', array(), filemtime( PDEV_DIR . 'public/css/tgwfchecker.css' ), 'all' );
-	wp_enqueue_style( 'tgwf-searchform', URL_DIR . 'public/css/tgwf-searchform.css', array(), '1.0', 'all' );
+	wp_enqueue_style( 'tgwf-searchform', URL_DIR . 'public/css/tgwf-searchform.css', array(), filemtime( PDEV_DIR . 'public/css/tgwf-searchform.css' ), 'all' );
 
 	// Enqueue scripts.
-	wp_enqueue_script( 'tgwf-app-link', URL_DIR . 'public/js/browserdetect.js' , array(), '1.0', true );  
+	wp_enqueue_script( 'tgwf-app-link', URL_DIR . 'public/js/browserdetect.js' , array(), '1.0', true );
 }
 
-
-
 /**
- * Load scripts and styles conditionally and async.
+ * Load styles on the condition the shortcode is one use in the page somewhere.
  */
-function tgwf_register_shortcode_styles() {
-
+function tgwf_load_shortcode_styles() {
 	global $post;
 
-	// Only load your scripts and styles if the post contains your shortcode.
-	if ( ! has_shortcode( $post->post_content, 'green-checker-search-form' ) ) :
-		return;
-	endif;
-
-	// Need this line when not working locally.
-	$response = wp_remote_get( URL_DIR . '/public/css/tgwf-searchform.css' );
-
-	// Need this line when working locally.
-	//$response = wp_remote_get( 'http://tgwf.local/wp-content/plugins/wp-green-checker/public/css/tgwf-searchform.css' );
-
-	if ( is_array( $response ) && ! is_wp_error( $response ) ) {
-		$headers = $response['headers']; // Array of http header lines.
-		$body    = $response['body']; // The content.
-		echo '<style>' . $body . '</style>';
+	if ( is_a( $post, 'WP_Post' ) && has_shortcode( $post->post_content, 'green-checker-search-form' ) ) {
+		wp_enqueue_style( 'tgwf-searchform', URL_DIR . 'public/css/tgwf-searchform.css', array(), filemtime( PDEV_DIR . 'public/css/tgwf-searchform.css' ), 'all' );
 	}
 }
