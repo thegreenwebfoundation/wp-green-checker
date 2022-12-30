5255b72dc4f7289948828f2b662e2306abe5f36f
hanopcan <hannah@opcan.co.uk>
2022-12-30 17:27:03 +0000

    Update search form with new brand

--- templates/checker/checker-search-form.php
+++ templates/checker/checker-search-form.php
@@ -8,15 +8,22 @@
 ?>
 
 <div class="tgwf-search-form__wrapper">
-	<div class="tgwf-search-form">
-		<h1>Is your website hosted <span style="color: #55AA3C">green</span>?</h1>
 
-		<p style="border-left: 6px solid #55AA3C; padding: 1rem; font-size: 1.1em; font-weight: bold;">One day the Internet will run entirely on renewable energy. The Green Web Foundation believes that day should be within reach, and develops tools to speed up the transition towards a green Internet</p>
+	<div class="tgwf-image">
+		<img width="200" height="200" src="<?php echo esc_url( URL_DIR ) . 'public/img/TGWF-recycle-green.svg'; ?>" alt="TGWF recycle green icon">
+	</div>
+
+	<div class="tgwf-search-form">
+		<h1>Is your website hosted green?</h1>
+
+		<p style="padding-bottom: 1rem;">One day the Internet will run entirely on renewable energy. The Green Web Foundation believes that day should be within reach, and develops tools to speed up the transition towards a green Internet.</p>
 
 		<?php // Form posts results back into the /green-web-check page URL, which runs tgwf_run_site_check(). ?>
 		<form action="<?php echo esc_url( get_bloginfo( 'url' ) ) . '/green-web-check'; ?>" method="GET">
 			<input name="url" class="form-control" type="text" placeholder="https://www.yourwebsite.com">
-			<button type="submit" class="tgwf-search-form__button">Check&nbsp;&nbsp;<span class="fa fa-chevron-right"></span></button>
+			<div class="tgwf-search-form__button wp-block-button is-style-secondary">
+				<button type="submit" class="wp-block-button__link">Check</button>
+			</div>
 		</form>
 	</div>
 </div>
