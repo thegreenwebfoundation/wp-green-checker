07e1298e6224f79c6f95cba3c9430e1abfec44b7
hanopcan <hannah@opcan.co.uk>
2023-07-10 15:35:13 +0100

    Improve styling of results page, remove CTA

--- templates/checker/checker-check-again.php
+++ templates/checker/checker-check-again.php
@@ -5,37 +5,19 @@
  *  @package tgwfgreenchecker
  */
 
-if ( WP_Block_Patterns_Registry::get_instance()->is_registered( 'gwf/text-gradient-image' ) ) {
-	?>
+?>
 
-	<div class="wp-block-cover alignfull is-light pattern__text-gradient-image">
-		<img class="wp-block-cover__image-background wp-image-4106" alt="" src="https://tgwf.local/wp-content/uploads/two-protestors_newsprint.jpg" />
-		
-		<div class="wp-block-cover__inner-container">
-			<div class="wp-block-group has-background">
+<div class="wp-block-cover alignfull is-light gwf-check-again pattern__text-gradient-image">
+	
+	<div class="wp-block-cover__inner-container">
+		<div class="wp-block-group gwf-check-again__text">
+			<div class="wp-block-group__inner-container" style="margin-left: auto; margin-right: auto;">
 				<h2 class="has-neve-text-color-color has-text-color">Run another green check</h2>
 
 				<?php require_once PDEV_DIR . 'templates/checker/checker-search-form.php'; ?>	
 			</div>
 		</div>
 	</div>
-
-	<?php
-} else {
-	?>
-	<div class="tgwf-search-form tgwf-search-form__check-again wp-block-group alignfull has-nv-site-bg-color has-neve-link-hover-color-background-color has-text-color has-background">
-	
-	<div class="wp-block-group__inner-container" style="max-width: 789px;">
-
-		<h2><?php _e( 'Perform another green check','tgwf' ); ?></h2>	
-
-		<?php require_once PDEV_DIR . 'templates/checker/checker-search-form.php'; ?>	
-
-	</div>
-	<?php
-}
-?>
-
-
+</div>
 
 <?php
