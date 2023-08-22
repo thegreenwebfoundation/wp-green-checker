9c56e8e4ed74ad235b611a2c2f88d557e8defe45
hanopcan <hannah@opcan.co.uk>
2023-08-22 16:08:36 +0100

    Update CTA order and links

--- templates/checker/checker-ctas.php
+++ templates/checker/checker-ctas.php
@@ -15,14 +15,15 @@
 
 		<div class="gwf-app-cta__ctas wp-block-gwf__threecols-t-p-b">
     
-			<h3 class="gwf__threecols-t-p-b__title gwf-iconbg-sunburst--black">Spread<br/>the word</h3>
-			<p class="gwf__threecols-t-p-b__para">When you use our open source work and cite us publically it helps us show our impact to funders.</p>
+			<h3 class="gwf__threecols-t-p-b__title gwf-iconbg-lightning--black">Support<br/>the mission</h3>
+			<p class="gwf__threecols-t-p-b__para">We are a non-profit working for the public benefit, with more funding we can have more impact.</p>
 
-			<div class="wp-block-button is-style-secondary gwf__threecols-t-p-b__button mt-0">
-				<a class="wp-block-button__link" href="https://www.thegreenwebfoundation.org/services/">
-				How to cite our work
+			<div class="wp-block-button is-style-primary gwf__threecols-t-p-b__button mt-0">
+				<a class="wp-block-button__link" href="https://www.thegreenwebfoundation.org/donate">
+				How donating works
 				</a>
 			</div>
+
 
 			<h3 class="gwf__threecols-t-p-b__title gwf-iconbg-plant--black">Share your<br/>suggestions</h3>
 			<p class="gwf__threecols-t-p-b__para">We welcome your feedback and perspectives on how to make this Green Web Checker tool better.</p>
@@ -33,12 +34,12 @@
 				</a>
 			</div>
 
-			<h3 class="gwf__threecols-t-p-b__title gwf-iconbg-lightning--black">Support<br/>the mission</h3>
-			<p class="gwf__threecols-t-p-b__para">We are a non-profit working for the public benefit, with more funding we can have more impact.</p>
+			<h3 class="gwf__threecols-t-p-b__title gwf-iconbg-sunburst--black">Spread<br/>the word</h3>
+			<p class="gwf__threecols-t-p-b__para">When you use our open source work and cite us publically it helps us show our impact to funders.</p>
 
-			<div class="wp-block-button is-style-primary gwf__threecols-t-p-b__button mt-0">
-				<a class="wp-block-button__link" href="https://www.thegreenwebfoundation.org/donate">
-				How donating works
+			<div class="wp-block-button is-style-secondary gwf__threecols-t-p-b__button mt-0">
+				<a class="wp-block-button__link" href="https://www.thegreenwebfoundation.org/how-to-cite-our-work/">
+				How to cite our work
 				</a>
 			</div>
 
