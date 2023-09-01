5d52fa4912f5c5a41259fc3bde1775da50b4c0ee
hanopcan <hannah@opcan.co.uk>
2023-09-01 11:17:34 +0100

    Update CTA text

--- templates/checker/checker-ctas.php
+++ templates/checker/checker-ctas.php
@@ -11,12 +11,12 @@
 	<div class="wp-block-group__inner-container" style="margin-bottom: 0;">
 
 		<h2 class="gwf-app-cta__title">Found our Green Web Check useful?</h2>
-		<p class="gwf-app-cta__intro">Help speed up the transition towards a greener internet by supporting the mission.</p>
+		<p class="gwf-app-cta__intro">Here's how you can speed up the transition to a greener internet.</p>
 
 		<div class="gwf-app-cta__ctas wp-block-gwf__threecols-t-p-b">
     
 			<h3 class="gwf__threecols-t-p-b__title gwf-iconbg-lightning--black">Support<br/>the mission</h3>
-			<p class="gwf__threecols-t-p-b__para">We are a non-profit working for the public benefit, with more funding we can have more impact.</p>
+			<p class="gwf__threecols-t-p-b__para">Help sustain this work as an open source project in service to the public’s interest.</p>
 
 			<div class="wp-block-button is-style-primary gwf__threecols-t-p-b__button mt-0">
 				<a class="wp-block-button__link" href="https://www.thegreenwebfoundation.org/donate">
@@ -26,7 +26,7 @@
 
 
 			<h3 class="gwf__threecols-t-p-b__title gwf-iconbg-plant--black">Share your<br/>suggestions</h3>
-			<p class="gwf__threecols-t-p-b__para">We welcome your feedback and perspectives on how to make this Green Web Checker tool better.</p>
+			<p class="gwf__threecols-t-p-b__para">We welcome your feedback on how to make our tools better.</p>
 
 			<div class="wp-block-button gwf__threecols-t-p-b__button mt-0">
 				<a class="wp-block-button__link" href="https://www.thegreenwebfoundation.org/support-form">
@@ -35,7 +35,7 @@
 			</div>
 
 			<h3 class="gwf__threecols-t-p-b__title gwf-iconbg-sunburst--black">Spread<br/>the word</h3>
-			<p class="gwf__threecols-t-p-b__para">When you use our open source work and cite us publically it helps us show our impact to funders.</p>
+			<p class="gwf__threecols-t-p-b__para">When you cite us publicly, it helps more people discover this work and take action.</p>
 
 			<div class="wp-block-button is-style-secondary gwf__threecols-t-p-b__button mt-0">
 				<a class="wp-block-button__link" href="https://www.thegreenwebfoundation.org/how-to-cite-our-work/">
