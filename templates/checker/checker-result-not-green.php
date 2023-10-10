17fc5c8bec36396bc44de306fc972de5ab9591bc
hanopcan <hannah@opcan.co.uk>
2023-10-10 14:34:44 +0100

    Improve layout of result error page

--- templates/checker/checker-result-not-green.php
+++ templates/checker/checker-result-not-green.php
@@ -26,7 +26,7 @@
 				<p class="tgwf-result__result">
 					<span class="tgwf-result__text--feedback">Oh dear!</span>
 					<span class="tgwf-result__text--url"><?php echo esc_html( $green_check_result->url ); ?></span>
-					<span class="tgwf-result__text--hosting-outcome">is not hosted green</span>
+					<span class="tgwf-result__text--hosting-outcome">is probably not hosted green</span>
 				</p>
 			</div>
 		</div>
@@ -36,49 +36,47 @@
 			</div>
 
 			<div class="tgwf-result__hosted-by">
-				<p>Unfortunately, this website is hosted grey or we are not sure about the greenness of the hoster.</p>
-				<p>For the check to work, (technical) information about green hosters worldwide is needed. This information is stored in our <a href="https://www.thegreenwebfoundation.org/green-web-datasets/">Green Web Dataset</a>.</p>
+				<p>Unfortunately, we can't find any evidence in our <a href="https://www.thegreenwebfoundation.org/green-web-dataset/">Green Web Dataset</a> that this website runs on green electricity.</p>
+				<p class="mb-0">For a check to return a green result, the website's hosting provider needs to submit evidence to demonstrate what steps they are taking to avoid, reduce or offset the emissions caused by the digital infrastructure they use.</p>
 			</div>
 		</div>
 	</div>
 </div>
 
-<div class="">
+<div class="wp-block-group alignfull mt-0">
+	<div class="wp-block-group__inner-container" style="max-width: 789px; padding-left: 0; padding-right: 0;">
 
-	<div class="wp-block-group alignfull">
-		<div class="wp-block-group__inner-container" style="max-width: 789px;">
+		<h2 class="">Want to turn this result green?</h2>
+		<p>Here's how you can turn that frown upside down...</p>
 
-			<h2 class="">Want to green this result?</h2>
-			<p>Here's how you can turn that frown upside down...</p>
-
-			<div class="wp-block-columns" style="gap: 2rem; margin-top: 2rem;">
-				<div class="wp-block-column" style="display: flex; flex-direction: column;">
-					<h3>For website owners</h3>
-					<p style="flex-grow: 1;"><span style="font-weight: 700">Share this result with your hosting provider.</span> Talk to your hosting provider and ask them to work with us so we can gather data and evidence about their renewable energy use. You can use our sample emails to help you raise the issue.</p>
-				
-					<div class="wp-block-buttons">
-						<div class="wp-block-button">
-							<a class="wp-block-button__link" href="https://www.thegreenwebfoundation.org/sample-emails/">View sample emails</a>
-						</div>
+		<div class="wp-block-columns" style="gap: 2rem; margin-top: 2rem;">
+			<div class="wp-block-column" style="display: flex; flex-direction: column;">
+				<h3>For website owners</h3>
+				<p style="flex-grow: 1; padding-top: 0;"><span style="font-weight: 700">Share this result with your hosting provider.</span> Talk to your hosting provider and ask them to work with us so we can gather data and evidence about their renewable energy use. You can use our sample emails to help you raise your concerns.</p>
+			
+				<div class="wp-block-buttons">
+					<div class="wp-block-button">
+						<a class="wp-block-button__link" href="https://www.thegreenwebfoundation.org/sample-emails/">View sample emails</a>
 					</div>
-
 				</div>
 
-				<div class="wp-block-column" style="display: flex; flex-direction: column;">
-					<h3>For hosting providers</h3>
-					<p style="flex-grow: 1;"><span style="font-weight: 700;">Submit data or corrections.</span> If you think this data is incorrect and would like to query or update it, read our guide for an explanation and next steps.</p>
-	
-					<div class="wp-block-buttons">
-						<div class="wp-block-button">
-							<a class="wp-block-button__link" href="https://www.thegreenwebfoundation.org/support/why-does-my-website-show-up-as-grey-in-the-green-web-checker/">Read guide</a>
-						</div>
+			</div>
+
+			<div class="wp-block-column" style="display: flex; flex-direction: column;">
+				<h3>For hosting providers</h3>
+				<p style="flex-grow: 1; padding-top: 0;"><span style="font-weight: 700;">Submit data or corrections.</span> If you think this data is incorrect and would like to query or update it, read our guide for an explanation and next steps.</p>
+
+				<div class="wp-block-buttons">
+					<div class="wp-block-button">
+						<a class="wp-block-button__link" href="https://www.thegreenwebfoundation.org/support/why-does-my-website-show-up-as-grey-in-the-green-web-checker/">Read guide</a>
 					</div>
-				
 				</div>
+			
 			</div>
 		</div>
 	</div>
-
 </div>
+
+
 
 <?php
