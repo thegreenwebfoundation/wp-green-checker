9063d8f5ed4c89f7b5ad0329da8fea8f09ad21dd
hanopcan <hannah@opcan.co.uk>
2024-01-26 11:15:36 +0000

    Remove Directory related files

--- src/enqueue.php
+++ src/enqueue.php
@@ -14,28 +14,9 @@ add_action( 'wp_enqueue_scripts', 'tgwf_load_shortcode_styles' );
  */
 function tgwf_register_scripts() {
 
-	if ( is_page( 'directory' ) ) {
-		add_action( 'wp_enqueue_scripts', 'tgwf_enqueue_directory_scripts' );
-	}
-
 	if ( is_page( 'green-web-check' ) ) {
 		add_action( 'wp_enqueue_scripts', 'tgwf_enqueue_greencheck_scripts' );
 	}
-}
-
-/**
- * Loads the scripts and styles required for the directory page.
- */
-function tgwf_enqueue_directory_scripts() {
-	// Enqueue styles.
-	wp_enqueue_style( 'tgwf-directory', URL_DIR . 'public/css/directory.css', array(), filemtime( PDEV_DIR . 'public/css/directory.css' ), 'all' );
-	wp_enqueue_style( 'tgwf-jqvmap', URL_DIR . 'public/css/jqvmap.css', array(), '1.0', 'all' );
-
-	// Enqueue scripts.
-	wp_enqueue_script( 'tgwf-directory', URL_DIR . 'public/js/directory.js', array( 'jquery' ), '1.0', true );
-	wp_enqueue_script( 'tgwf-scripts', URL_DIR . 'public/js/tgwf.js', array( 'jquery' ), '', false );
-	wp_enqueue_script( 'tgwf-vmap', URL_DIR . 'public/js/jquery.vmap.js', array( 'jquery' ), '1.0', true );
-	wp_enqueue_script( 'tgwf-vmap-world', URL_DIR . 'public/js/maps/jquery.vmap.world.js', array( 'jquery' ), '1.0', true );
 }
 
 
